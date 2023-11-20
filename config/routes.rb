865879c7df447177ccff79102ba3afe102976e08
Arbi Velaj <arbi.velaj@gmail.com>
2023-11-20 18:06:03 +0100

    emmet lovers

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "flats#index"
+
+  get "/flats", to: "flats#index"
+
+  get "flats/:id", to: "flats#show"
+
+  # verb , path, controller action
+end
